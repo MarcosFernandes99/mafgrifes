38227ca7986b5db79f0d458d0b77d94d5824fba9
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2023-10-02 13:23:06 -0300

    estrutura montada

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,28 @@
+import "../app/globals.css"
+import ButtonAction from "@/components/ButtonAction"
+import Image from "next/image"
+import logo from "../public/logo.png"
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
-  }
+  return (
+    <>
+      <main className="flex justify-center w-full h-screen items-center">
+
+        <div className="min-w-1/5 h-96 rounded-xl shadow-xl bg-white flex flex-col items-center">
+          <div className="mt-5 h-16 w-4/5 flex justify-between ">
+            <span className="font-bold text-2xl">MAF - Grifes</span>
+            <Image className="h-16 w-16" src={logo} alt="Logo"></Image>
+          </div>
+          <h3 className="w-3/5 mt-5 text-xl text-center">Seja bem-vindo à nossa loja! 😍</h3>
+
+          <div className="mt-5 items-center flex flex-col w-full">
+            <ButtonAction label="Whatsapp" />
+            <ButtonAction label="Catálogo" />
+            <ButtonAction label="Pedido atacado" />
+          </div>
+        </div>
+
+      </main>
+    </>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,20 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
+  theme: {   
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        "slate": "#f8fafc",
+        "slate100": "#f1f5f9",
+      },
+      minWidth: {
+        "1/5": "20%"
+      }
     },
   },
   plugins: [],
